83b06ca86e5c57dbf0b615cec292ee843eb70227
govin08 <govindah08@gmail.com>
2025-05-21 15:19:05 +0900

    todo and batch

---
+++ git_config.bat
@@ -0,0 +1,12 @@
+@echo off
+chcp 949
+echo Git 사용자 정보 설정 중...
+
+git config --global user.email "govindah08@gmail.com"
+git config --global user.name "govin08"
+
+echo 설정 완료!
+echo 이메일: govindah08@gmail.com
+echo 사용자: govin08
+
+pause
